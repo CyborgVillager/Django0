e056c7335abb2942c581326c3db57dce6fb3b932
CyborgVillager <almawijonathan@gmail.com>
2020-03-19 03:43:35 -0500

    v0 Django Respo Created
    Django Respo Created: Respo created, Django Project
    
                                Files Changed:
                                * Installation of Django & its components
                                * venv
                                * README & Version_Info files
    
                                Objectives:
                                * Basic Intro to Django/build upon previous builds/improve concepts & modularity for
                                sites & future projects

---
+++ django0_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\PyProject\GitRespo\Web Dev\Full Stack\Django0\Django0\django0_venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
