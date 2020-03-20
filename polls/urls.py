493557f7cdb6ee4f7e237bfb533445312fc6bf78
CyborgVillager <almawijonathan@gmail.com>
2020-03-20 01:45:21 -0500

    v4 Voting Form/CSS Update
    Voting Form/CSS Update: updated the following filees
    
                                Files Changed:
                                    * Updated detail.html
                                    * Updated styles0.css to include button style
                                    * Updated urls.py
                                    * Updated views.py -> deleted & reformed the file/compress data
                                    * New file added results.html once the poll has been voted/be redirected to that page
    
                                Summary: Poll App is taking form with added features & functions
    
                                Objectives: Auto-tests , make the site mobile friendly

--- polls/urls.py
+++ polls/urls.py
@@ -1,21 +1,17 @@
 from django.urls import path
-
+from . import views
 # Try to import views.py info for polls
 try:
     from .import views
     app_name = 'polls'
+
     urlpatterns = [
         # e.g URL: url/polls
         # -> Hello this is the poll index
-        path('', views.index, name='index'),
-        # e.g URL: /polls/#/
-        # -> You are currently looking at question #
-        path('<int:question_id>/', views.detail, name='detail'),
-        # e.g URL: /polls/#/results/
-        # -> Result for question #
-        path('<int:question_id>/results/', views.results, name='results'),
-        # e.g URL: /polls/#/vote/
-        # -> You've voted for question #
+
+        path('', views.IndexView.as_view(), name='index'),
+        path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+        path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
         path('<int:question_id>/vote/', views.vote, name='vote'),
     ]
     print('.import views success @ polls/urls.py')
