6f5c593f1c9b77b8acc7ca6f1791682c8c61f78a
CyborgVillager <almawijonathan@gmail.com>
2020-03-21 07:54:59 -0500

    v7 Admin Page Modification & Front End Info
    Admin Page Modification & Front End Info: modified admin to allow options of choices/ more data to help
                                                    the admin make decisions on poll w/date/info/ check if it was published recently
    
                                                   Files Changed:
                                                    * polls/admin.py
                                                        - added info to help admin's functionality on the site
                                                    * polls/models.py
                                                        - helps admin end on seeing the published date/recent
                                                    * detail.html
                                                        -> added classes to be used @ style0.css
                                                            - class="options"
                                                            - class="voted"
                                                    * index.html
                                                    * results.html
                                                        -> Added classes to be used @ style0.css
                                                            - class="results"
                                                            - class="votetext"
                                                            - class="voteoption"
                                                    * style0.css
                                                        - .options
                                                        - .voted
                                                        - .votetext
                                                        - .voteoption
                                                        - .results
    
                                     Summary: Improved Site functionality on both front/backend, admins now have more info
                                                to help them on improving the poll's experience for the user
    
                                                User now have data to see on the # of votes for x,y,z choices
                                                Visual Aid to assist user
    
                                     Objectives: Respo completed, will be working on a mini proj as a reference point.

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,33 @@
 from django.contrib import admin
 
+
+
 # Question Imports
 try :
-    from .models import Question
-    admin.site.register(Question)
+    from .models import Question,Choice
+    # Question choices , can modify # of questions from -> extra
+    class ChoiceInline(admin.TabularInline):
+        model = Choice
+        extra = 3
+
+    class QuestionAdmin(admin.ModelAdmin):
+        # fields of question
+        fieldsets = [
+            (None, {'fields': ['question_text']}),
+            ('Date information', {'fields': ['published_date'], 'classes': ['collapse']}),
+        ]
+        inlines = [ChoiceInline]
+
+        # Shows Date published info / check if it was published recently on the questions txt list
+        list_display = ('question_text', 'published_date', 'was_published_recently')
+        # symbol / connects to models.py
+        list_filter = ['published_date']
+        # search for questions bar
+        search_fields = ['question_text']
+    admin.site.register(Question, QuestionAdmin)
+
+
+
     print('Polls Admin Module - Question Import Success')
 except ImportError:
     print('Unable to import .models import Question.' + '\n' +
